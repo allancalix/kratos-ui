5266614a6d39a4112b96325e4e01b75cfc17d580
Allan Calix <acalix0592@gmail.com>
2021-06-03 19:55:12 -0700

    Fixes typo

--- jest-e2e.config.js
+++ jest-e2e.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testMatch: [
     "**/e2e/*.test.js?(x)",
   ],
-  setupFileAfterEnv: ['./jest.setup.js'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
   preset: 'jest-playwright-preset',
 }
