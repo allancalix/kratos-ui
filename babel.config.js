657fb552d710f051be33c184d44e7b6d2052f8cb
Allan Calix <acalix0592@gmail.com>
2021-06-09 01:55:27 -0700

    Updates to ES module

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    test: {
-      plugins: ["transform-es2015-modules-commonjs"]
-    }
-  }
-};
--- jest-unit.config.js
+++ jest-unit.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   testEnvironment: "jsdom",
   testMatch: [
-    "**/__tests__/**/*.js?(x)",
+    "**/__tests__/**/*.mjs",
   ],
   verbose: true,
+  transform: {
+    "\\.m?[jt]sx?$": "babel-jest"
+  },
   transformIgnorePatterns: [
-    "/node_modules/(?!@glennsl/bs-jest|bs-platform).+\\.js$"
+    "/node_modules/(?!@glennsl/bs-jest|bs-platform).+\\.js$",
+    "/node_modules/rescript/.*",
+  ],
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "jsx",
+    "node",
+    "mjs"
   ],
 }
